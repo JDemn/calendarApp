59715c862a3e94955ced40b236f4dd5873cbaa55
JDemn <dsoftwaremancilla@outlook.es>
2021-12-11 22:15:43 -0600

    customBox in calendar, events of calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+
 import { Navbar } from '../ui/Navbar';
+import { CalendarEvent } from './CalendarEvent';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 
 const localizer = momentLocalizer(moment) // or globalizeLocalizer
 
@@ -12,10 +16,27 @@ const events = [{
     start   : moment().toDate(), //manejo de fechas con moment
     end     : moment().add(2,'hours').toDate(), //este evento se acaba en dos horas 
     bgcolor : '#fafafa',
-    notes   : 'buy the cupe cake'
+    notes   : 'buy the cupe cake',
+    user    : {
+        _id     : '123',
+        name    : 'José'
+    }
 }]
 export const CalendarScreen = () => {
+    const [lastView, setlastView] = useState(localStorage.getItem('lastView') || 'month');
 
+    //estar pendientes de acciones que van a suceder y reaccionar ante ellas
+    const onDoubleClick = (e)=> {
+        console.log(e);
+    }
+    const onSelectEvent =(e)=> {
+        console.log(e)
+    }
+    const onViewChange =(e)=> {
+        setlastView(e)
+        // console.log(e)
+        localStorage.setItem('lastView', e );
+    }
     const eventStyleGetter =( event, start, end, isSelected )=>{
         console.log(event, start, end, isSelected);
         const style = {
@@ -34,12 +55,22 @@ export const CalendarScreen = () => {
             <Navbar />
 
             <Calendar
-                localizer={ localizer }
-                events={ events }
-                startAccessor="start"
-                endAccessor="end"
-                className='p-4'
-                eventPropGetter={ eventStyleGetter } //dispara la función eventStyleGetter 
+                localizer = { localizer }
+                events = { events }
+                startAccessor ="start"
+                endAccessor = "end"
+                className = 'p-4'
+                eventPropGetter = { eventStyleGetter } //dispara la función eventStyleGetter 
+                onDoubleClickEvent = { onDoubleClick }
+                onSelectEvent = { onSelectEvent }
+                onView = { onViewChange }
+                view = { lastView }
+                //mostrar un componente externo como referencia
+                components={
+                    {
+                        event : CalendarEvent
+                    }
+                }
             />
         </div>
     )
